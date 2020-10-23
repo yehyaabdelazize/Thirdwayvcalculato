c288ca0217aed49f97d78c50c19c77cd68fb6e1e
Rafiiq <android1@codecaique.com>
2020-10-23 19:02:37 +0200

    initial commit

---
+++ src/main/java/com/yahya/thirdwayvcalculator/ui/MainCalcutor/CalcuActivity.java
@@ -0,0 +1,17 @@
+package com.yahya.thirdwayvcalculator.ui.MainCalcutor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yahya.thirdwayvcalculator.R;
+
+public class CalcuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calcu);
+
+    }
+}
---
+++ src/main/java/com/yahya/thirdwayvcalculator/ui/Splash/Splash.java
@@ -0,0 +1,37 @@
+package com.yahya.thirdwayvcalculator.ui.Splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import com.yahya.thirdwayvcalculator.R;
+import com.yahya.thirdwayvcalculator.ui.MainCalcutor.CalcuActivity;
+
+public class Splash extends AppCompatActivity {
+
+    private static int timeOut =3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent nextIntent = new Intent(Splash.this, CalcuActivity.class);
+                startActivity(nextIntent);
+                finish();
+            }
+        },timeOut);
+
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+    }
+}
