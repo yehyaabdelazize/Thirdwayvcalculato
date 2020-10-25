f20b89bd258f7c6b50455e723adeca66c6a3e6ff
Rafiiq <android1@codecaique.com>
2020-10-25 12:48:14 +0200

    2nd Commit

--- src/main/java/com/yahya/thirdwayvcalculator/ui/MainCalcutor/CalcuActivity.java
+++ src/main/java/com/yahya/thirdwayvcalculator/ui/MainCalcutor/CalcuActivity.java
@@ -2,38 +2,54 @@ package com.yahya.thirdwayvcalculator.ui.MainCalcutor;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 
 import com.yahya.thirdwayvcalculator.R;
 import com.yahya.thirdwayvcalculator.databinding.ActivityCalcuBinding;
 import com.yahya.thirdwayvcalculator.databinding.ActivitySplashBinding;
+import com.yahya.thirdwayvcalculator.pojo.models.OperationModel;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CalcuActivity extends AppCompatActivity {
 
     ActivityCalcuBinding calcuBinding;
-    double num;
     double result = 0;
+    double num2;
+    List<OperationModel> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         calcuBinding = DataBindingUtil.setContentView(this, R.layout.activity_calcu);
 
-        double result = 0;
+        // initialling variable Result on starting with zero (0.0)
         calcuBinding.tvResult.setText("" + result);
 
+
+//        list = new ArrayList<>();
+//        CalcuAdapter adapter = new CalcuAdapter(this, list);
+//        calcuBinding.recOpers.setLayoutManager(new GridLayoutManager(this, 3));
+//
 
         // impl on clicking button ( + )
         calcuBinding.btnPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcuBinding.tvSecOpr.setText(calcuBinding.btnPlus.getText().toString());
+                calcuBinding.tvOper.setText(calcuBinding.btnPlus.getText().toString() + "  ");
                 calcuBinding.btnMinus.setEnabled(false);
                 calcuBinding.btnMultyply.setEnabled(false);
                 calcuBinding.btnDivision.setEnabled(false);
+                calcuBinding.tvOper.setVisibility(View.VISIBLE);
+                calcuBinding.etSecOper.setHint("");
             }
         });
 
@@ -41,11 +57,12 @@ public class CalcuActivity extends AppCompatActivity {
         calcuBinding.btnMinus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcuBinding.tvSecOpr.setText(calcuBinding.btnMinus.getText().toString());
+                calcuBinding.tvOper.setText(calcuBinding.btnMinus.getText().toString() + "  ");
                 calcuBinding.btnPlus.setEnabled(false);
                 calcuBinding.btnMultyply.setEnabled(false);
                 calcuBinding.btnDivision.setEnabled(false);
-                view.setFocusableInTouchMode(false);
+                calcuBinding.tvOper.setVisibility(View.VISIBLE);
+                calcuBinding.etSecOper.setHint("");
             }
         });
 
@@ -54,11 +71,13 @@ public class CalcuActivity extends AppCompatActivity {
         calcuBinding.btnMultyply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcuBinding.tvSecOpr.setText(calcuBinding.btnMultyply.getText().toString());
+                calcuBinding.tvOper.setText(calcuBinding.btnMultyply.getText().toString() + "  ");
                 calcuBinding.btnMinus.setEnabled(false);
                 calcuBinding.btnPlus.setEnabled(false);
                 calcuBinding.btnDivision.setEnabled(false);
-                view.setFocusableInTouchMode(false);
+                calcuBinding.tvOper.setVisibility(View.VISIBLE);
+                calcuBinding.etSecOper.setHint("");
+
             }
         });
 
@@ -66,39 +85,56 @@ public class CalcuActivity extends AppCompatActivity {
         calcuBinding.btnDivision.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcuBinding.tvSecOpr.setText(calcuBinding.btnDivision.getText().toString());
+                calcuBinding.tvOper.setText(calcuBinding.btnDivision.getText().toString() + "  ");
                 calcuBinding.btnMinus.setEnabled(false);
                 calcuBinding.btnPlus.setEnabled(false);
                 calcuBinding.btnMultyply.setEnabled(false);
-                view.setFocusableInTouchMode(false);
+                calcuBinding.tvOper.setVisibility(View.VISIBLE);
+                calcuBinding.etSecOper.setHint("");
+
             }
         });
 
-
-        // impl on clicking button ( = )
+        // impl on clicking button ( / )
         calcuBinding.btnEqual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                num = Double.parseDouble(calcuBinding.etNum2.getText().toString());
-                double res;
-                switch (calcuBinding.tvSecOpr.getText().toString()) {
-                    case "+":
-                        res = num +result;
-                        calcuBinding.tvResult.setText(""+res);
-                    case "-":
-                        res = num -result;
-                        calcuBinding.tvResult.setText(""+res);
-                    case "*":
-                        res = num *result;
-                        calcuBinding.tvResult.setText(""+res);
-                    case "/":
-                        res = num /result;
-                        calcuBinding.tvResult.setText(""+res);
-                }
+                calcuBinding.btnMinus.setEnabled(false);
+                calcuBinding.btnPlus.setEnabled(false);
+                calcuBinding.btnMultyply.setEnabled(false);
+                calcuBinding.btnDivision.setEnabled(false);
+
+                calcu_operation();
+
 
             }
         });
 
+    }
 
+    //impl of calculation the operation for the Result
+    public void calcu_operation() {
+
+        String op = calcuBinding.tvOper.getText().toString();
+        num2 = Double.parseDouble(calcuBinding.etSecOper.getText().toString());
+
+        switch (op) {
+            case "+":
+                result += num2;
+                calcuBinding.tvResult.setText("" + result);
+                break;
+            case "-":
+                result -= num2;
+                calcuBinding.tvResult.setText("" + result);
+                break;
+            case "*":
+                result *= num2;
+                calcuBinding.tvResult.setText("" + result);
+                break;
+            case "/":
+                result /= num2;
+                calcuBinding.tvResult.setText("" + result);
+                break;
+        }
     }
 }
---
+++ src/main/java/com/yahya/thirdwayvcalculator/ui/MainCalcutor/CalcuAdapter.java
@@ -0,0 +1,54 @@
+package com.yahya.thirdwayvcalculator.ui.MainCalcutor;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.yahya.thirdwayvcalculator.R;
+import com.yahya.thirdwayvcalculator.pojo.models.OperationModel;
+
+import java.util.List;
+
+public class CalcuAdapter extends RecyclerView.Adapter<CalcuAdapter.ViewHolder> {
+
+    private Context context;
+    private List<OperationModel> operations;
+
+    public CalcuAdapter(Context context, List<OperationModel> operationModels) {
+        this.context = context;
+        this.operations = operationModels;
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView operation;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            operation = itemView.findViewById(R.id.tv_rec_operation_item);
+        }
+    }
+
+    @Override
+    public CalcuAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v;
+        v = LayoutInflater.from(context).inflate(R.layout.item_operation_rec, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(CalcuAdapter.ViewHolder viewHolder, int i) {
+        viewHolder.operation.setText(operations.get(i).toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return operations.size();
+    }
+}
+
